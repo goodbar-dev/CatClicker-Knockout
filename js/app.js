1a85d54ad6953e40ebe25d62fbf3c5d71e77acec
goodbar <goodbardevelopment@outlook.com>
2017-12-11 16:06:17 -0600

    updated to use knockout.js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var ViewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.level = ko.computed(function() {
+    if (this.clickCount() <= 5) {
+        return "Baby";
+    } else if (this.clickCount() <= 10) {
+        return "Kid";
+    } else if (this.clickCount() <= 15) {
+        return "Teen";
+    } else if (this.clickCount() <= 20) {
+        return "Adult"
+    }
+  }, this);
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
+}
+
+ko.applyBindings(new ViewModel());
