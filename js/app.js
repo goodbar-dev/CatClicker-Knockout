726bcf56d091654dd8c44f7ea240938ba306cb62
goodbar <goodbardevelopment@outlook.com>
2017-12-11 16:26:02 -0600

    Split out cat into its own variable so that we can eventually add more cats.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ViewModel = function() {
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.level = ko.computed(function() {
@@ -15,8 +15,13 @@ var ViewModel = function() {
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
   this.catNicknames = ko.observableArray(['Zowie', 'Zoey', 'Zoie']);
+}
+
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Cat());
+
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   };
 }
 
